58497b21dbd9f115e012b99533ee5d5b6ad49a3d
Borys <rudashi@gmail.com>
2024-03-05 07:14:20 +0100

    Added FluentBuilder properties

--- src/FluentBuilder.php
+++ src/FluentBuilder.php
@@ -7,6 +7,27 @@ namespace Rudashi;
 class FluentBuilder
 {
     protected string $context = '';
+    /**
+     * @var array<int, string>
+     */
+    protected array $pattern = [];
+    /**
+     * @var array<int, string>
+     */
+    protected array $prefix = ['/'];
+    /**
+     * @var array<int, string>
+     */
+    protected array $suffix = ['/'];
+    /**
+     * @var array<int, string>
+     */
+    protected array $modifiers = [];
+
+    public function get(): string
+    {
+        return implode('', [...$this->prefix, ...$this->pattern, ...$this->suffix, ...$this->modifiers]);
+    }
 
     public function setContext(string $string): static
     {
