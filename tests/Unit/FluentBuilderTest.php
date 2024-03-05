8e863afb7c73efd9144a77f4d6c6c0456fc846ed
Borys <rudashi@gmail.com>
2024-03-05 07:15:02 +0100

    Added FluentBuilder test

---
+++ tests/Unit/FluentBuilderTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rudashi\FluentBuilder;
+
+function fluentBuilder(): FluentBuilder
+{
+    return new FluentBuilder();
+}
+
+it('can add context to the builder', function () {
+    $regex = fluentBuilder()->setContext('test');
+
+    try {
+        $reflectProperty = (new ReflectionClass($regex))->getProperty('context')->getValue($regex);
+
+        expect($reflectProperty)->toBe('test');
+    } catch (ReflectionException $e) {
+        $this->markTestSkipped($e->getMessage());
+    }
+});
+
+/**
+ * Returning results
+ */
+it('can return a string pattern', function () {
+    $regex = fluentBuilder();
+
+    expect($regex->get())
+        ->toBe('//');
+});
