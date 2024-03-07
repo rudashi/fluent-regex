3deaa2e5b9efcfc99edbaf4b7d0f6b2fd8533c0f
rudashi <rudashi@gmail.com>
2024-03-07 11:29:21 +0100

    Added negations to builder

--- src/Negate.php
+++ src/Negate.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Rudashi;
 
+use LogicException;
+
 /**
  * @mixin \Rudashi\FluentBuilder
  */
@@ -12,5 +14,35 @@ class Negate
     public function __construct(
         private FluentBuilder $builder,
     ) {
+    }
+
+    public function letter(): FluentBuilder
+    {
+        $this->builder->pushToPattern('[^a-zA-Z]');
+
+        return $this->builder;
+    }
+
+    /**
+     * @param  string  $method
+     * @param  array<int|string, mixed>  $arguments
+     * @return \Rudashi\FluentBuilder
+     */
+    public function __call(string $method, array $arguments): FluentBuilder
+    {
+        $this->builder->pushToPattern('[^');
+
+        $result = $this->builder->{$method}($arguments);
+
+        if ($result instanceof FluentBuilder) {
+            $this->builder->pushToPattern(']');
+
+            return $this->builder;
+        }
+
+        throw new LogicException(sprintf(
+            'Method "%s" is not extendable by "Negation".',
+            $method
+        ));
     }
 }
--- tests/Unit/NegateTest.php
+++ tests/Unit/NegateTest.php
@@ -5,6 +5,18 @@ declare(strict_types=1);
 use Rudashi\Negate;
 
 it('can create negation', function () {
-    expect(new Negate(fluentBuilder()))
+    expect(negation())
         ->toBeInstanceOf(Negate::class);
 });
+
+it('can call builder methods')
+    ->expect(negation()->letter()->get())
+    ->toBeString()
+    ->toBe('/[^a-zA-Z]/');
+
+it('thrown an exception if the property has no method assigned', function () {
+    expect(negation()->get());
+})->throws(
+    exception: LogicException::class,
+    exceptionMessage: 'Method "get" is not extendable by "Negation".'
+);
