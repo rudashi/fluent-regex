12aa7aaeb7afcda5051ed67d62323b40cc8f4d22
rudashi <rudashi@gmail.com>
2024-03-16 09:31:13 +0100

    Group negation tests

--- tests/Unit/NegateTest.php
+++ tests/Unit/NegateTest.php
@@ -38,89 +38,93 @@ it('thrown an exception if the property has no method assigned', function (strin
 /**
  * Built-in methods
  */
-it('returns the negation of letter', function () {
-    expect(negation()->letter()->get())
-        ->toBeString()
-        ->toBe('/[^a-zA-Z]/');
-});
+describe('Built-in methods', function () {
+    it('returns the negation of letter', function () {
+        expect(negation()->letter()->get())
+            ->toBeString()
+            ->toBe('/[^a-zA-Z]/');
+    });
 
-it('returns the negation of letters', function () {
-    expect(negation()->letters()->get())
-        ->toBeString()
-        ->toBe('/[^a-zA-Z]+/');
-});
+    it('returns the negation of letters', function () {
+        expect(negation()->letters()->get())
+            ->toBeString()
+            ->toBe('/[^a-zA-Z]+/');
+    });
 
-it('returns the negation of lowerLetter', function () {
-    expect(negation()->lowerLetter()->get())
-        ->toBeString()
-        ->toBe('/[^a-z]/');
-});
+    it('returns the negation of lowerLetter', function () {
+        expect(negation()->lowerLetter()->get())
+            ->toBeString()
+            ->toBe('/[^a-z]/');
+    });
 
-it('returns the negation of lowerLetters', function () {
-    expect(negation()->lowerLetters()->get())
-        ->toBeString()
-        ->toBe('/[^a-z]+/');
-});
+    it('returns the negation of lowerLetters', function () {
+        expect(negation()->lowerLetters()->get())
+            ->toBeString()
+            ->toBe('/[^a-z]+/');
+    });
 
     it('returns the negation of any', function () {
         expect(negation()->anyOf('abc')->get())
             ->toBeString()
             ->toBe('/[^abc]/');
     });
+});
 
 /**
  * Higher-Order methods
  */
-it('returns the negation of exactly', function () {
-    expect(negation()->exactly('foo bar')->get())
-        ->toBeString()
-        ->toBe('/[^foo bar]/');
-});
+describe('Higher-Order methods', function () {
+    it('returns the negation of exactly', function () {
+        expect(negation()->exactly('foo bar')->get())
+            ->toBeString()
+            ->toBe('/[^foo bar]/');
+    });
 
-it('returns the negation of whitespaces', function () {
-    expect(negation()->whitespace()->get())
-        ->toBeString()
-        ->toBe('/[^\s]/');
-});
+    it('returns the negation of whitespaces', function () {
+        expect(negation()->whitespace()->get())
+            ->toBeString()
+            ->toBe('/[^\s]/');
+    });
 
-it('returns the negation of non whitespaces', function () {
-    expect(negation()->nonWhitespace()->get())
-        ->toBeString()
-        ->toBe('/[^\S]/');
-});
+    it('returns the negation of non whitespaces', function () {
+        expect(negation()->nonWhitespace()->get())
+            ->toBeString()
+            ->toBe('/[^\S]/');
+    });
 
-it('returns the negation of digit', function () {
-    expect(negation()->digit()->get())
-        ->toBeString()
-        ->toBe('/[^\d]/');
-});
+    it('returns the negation of digit', function () {
+        expect(negation()->digit()->get())
+            ->toBeString()
+            ->toBe('/[^\d]/');
+    });
 
-it('returns the negation of digits', function () {
-    expect(negation()->digits()->get())
-        ->toBeString()
-        ->toBe('/[^\d+]/');
-});
+    it('returns the negation of digits', function () {
+        expect(negation()->digits()->get())
+            ->toBeString()
+            ->toBe('/[^\d+]/');
+    });
 
-it('returns the negation of non digit', function () {
-    expect(negation()->nonDigit()->get())
-        ->toBeString()
-        ->toBe('/[^\D]/');
-});
+    it('returns the negation of non digit', function () {
+        expect(negation()->nonDigit()->get())
+            ->toBeString()
+            ->toBe('/[^\D]/');
+    });
 
-it('returns the negation of non digits', function () {
-    expect(negation()->nonDigits()->get())
-        ->toBeString()
-        ->toBe('/[^\D+]/');
-});
+    it('returns the negation of non digits', function () {
+        expect(negation()->nonDigits()->get())
+            ->toBeString()
+            ->toBe('/[^\D+]/');
+    });
 
-it('returns the negation of word', function () {
-    expect(negation()->word()->get())
-        ->toBeString()
-        ->toBe('/[^\w]/');
-});
+    it('returns the negation of word', function () {
+        expect(negation()->word()->get())
+            ->toBeString()
+            ->toBe('/[^\w]/');
+    });
 
-it('returns the negation of words', function () {
-    expect(negation()->words()->get())
-        ->toBeString()
-        ->toBe('/[^\w+]/');
+    it('returns the negation of words', function () {
+        expect(negation()->words()->get())
+            ->toBeString()
+            ->toBe('/[^\w+]/');
+    });
 });
