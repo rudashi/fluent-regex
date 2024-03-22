0523e94416eff5dd518a4d1c4653a5e0e99b94b9
rudashi <rudashi@gmail.com>
2024-03-22 07:54:52 +0100

    Updated `Anchor` tests

--- tests/Unit/AnchorsTest.php
+++ tests/Unit/AnchorsTest.php
@@ -30,15 +30,35 @@ it('can add alias of `end` anchor', function () {
         ->toBe('/$/');
 });
 
+it('can add `start` and `end` anchor', function () {
+    $regex = fluentBuilder()->start()->end();
+
+    expect($regex->get())
+        ->toBe('/^$/');
+});
+
 it('thrown an exception when trying to negate the anchor', function (string $method) {
     expect(fn () => fluentBuilder()->not->{$method}())
         ->toThrow(
             exception: LogicException::class,
-            exceptionMessage: 'Method "'.$method.'" is not extendable by "Negation".'
+            exceptionMessage: 'Method "' . $method . '" is not extendable by "Negation".'
         );
 })->with([
     'start',
     'startOfLine',
     'end',
     'endOfLine',
+]);
+
+it('thrown an exception when trying to invoke anchor twice', function (string $method, string $called) {
+    expect(fn () => fluentBuilder()->{$method}()->{$method}())
+        ->toThrow(
+            exception: LogicException::class,
+            exceptionMessage: 'The "' . $called . '" anchor has already been called.'
+        );
+})->with([
+    ['start', 'start'],
+    ['startOfLine', 'start'],
+    ['end', 'end'],
+    ['endOfLine', 'end'],
 ]);
