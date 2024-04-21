cb8f0ea50675fc3da6853a58656f2f286e5cb6ef
rudashi <rudashi@gmail.com>
2024-04-21 07:59:20 +0200

    Removed unused test expectation

--- tests/Pest.php
+++ tests/Pest.php
@@ -29,10 +29,6 @@ uses(Tests\TestCase::class)->in('Feature');
 |
 */
 
-expect()->extend('toBeOne', function () {
-    return $this->toBe(1);
-});
-
 /*
 |--------------------------------------------------------------------------
 | Functions
