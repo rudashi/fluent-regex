9b89700e32a56944db287f96de9f841e82fb5da7
rudashi <rudashi@gmail.com>
2024-05-07 08:07:48 +0200

    New arch tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 namespace Tests;
 
 use Rudashi\Concerns\Dumpable;
+use Rudashi\Contracts\PatternContract;
 use Rudashi\Flag;
+
+arch('strict types are everywhere', function () {
+    expect('Rudashi')
+        ->toUseStrictTypes();
+});
 
 arch('globals', function () {
     expect(['dd', 'dump', 'die', 'var_dump', 'sleep'])
@@ -18,7 +24,18 @@ arch('traits', function () {
         ->toBeTraits();
 });
 
+arch('interfaces', function () {
+    expect('Rudashi\Contracts')
+        ->toBeInterfaces();
+});
+
 arch('enums', function () {
     expect(Flag::class)
         ->toBeStringBackedEnum();
+});
+
+arch('patterns', function () {
+    expect('Rudashi\Patterns')
+        ->toHaveSuffix('Pattern')
+        ->toOnlyImplement(PatternContract::class);
 });
