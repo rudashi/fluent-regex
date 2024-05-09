95e15e9274161fb9c9dfaf863fd925f426d06b07
rudashi <rudashi@gmail.com>
2024-05-09 07:07:20 +0200

    Added comments

--- src/Anchors.php
+++ src/Anchors.php
@@ -9,21 +9,36 @@ use LogicException;
 class Anchors
 {
     /**
+     * List of assigned starting anchors.
+     *
      * @var array<int, string>
      */
     protected array $prefix = [];
 
     /**
+     * List of assigned ending anchors.
+     *
      * @var array<int, string>
      */
     protected array $suffix = [];
 
+    /**
+     * Create an instance of Anchors.
+     *
+     * @param  \Rudashi\FluentBuilder  $builder
+     * @param  string  $delimiter
+     */
     public function __construct(
         private readonly FluentBuilder $builder,
         private readonly string $delimiter,
     ) {
     }
 
+    /**
+     * Adds a starting anchor.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
     public function start(): FluentBuilder
     {
         if ($this->prefix !== []) {
@@ -35,6 +50,11 @@ class Anchors
         return $this->builder;
     }
 
+    /**
+     * Adds an end anchor.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
     public function end(): FluentBuilder
     {
         if ($this->suffix !== []) {
@@ -47,6 +67,8 @@ class Anchors
     }
 
     /**
+     * Returns a list of starting anchors.
+     *
      * @return array<int, string>
      */
     public function getPrefix(): array
@@ -55,6 +77,8 @@ class Anchors
     }
 
     /**
+     * Returns a list of ending anchors.
+     *
      * @return array<int, string>
      */
     public function getSuffix(): array
@@ -62,6 +86,14 @@ class Anchors
         return [...$this->suffix, $this->delimiter];
     }
 
+    /**
+     * Throws a logical exception when the same flag is reused.
+     *
+     * @param  string  $method
+     * @return never
+     *
+     * @throws \LogicException
+     */
     protected function throwAnchorException(string $method): never
     {
         throw new LogicException(sprintf('The "%s" anchor has already been called.', $method));
--- src/Concerns/HasAnchors.php
+++ src/Concerns/HasAnchors.php
@@ -4,24 +4,48 @@ declare(strict_types=1);
 
 namespace Rudashi\Concerns;
 
+use Rudashi\FluentBuilder;
+
 trait HasAnchors
 {
-    public function start(): self
+    /**
+     * Adds the beginning of a string anchor.
+     * If multiline mode is used, this will also work after a newline character.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function start(): FluentBuilder
     {
         return $this->anchors->start();
     }
 
-    public function startOfLine(): self
+    /**
+     * Alias for the `start` method.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function startOfLine(): FluentBuilder
     {
         return $this->start();
     }
 
-    public function end(): self
+    /**
+     * Adds the end of a string anchor.
+     * If multiline mode is used, this will also work before a newline character.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function end(): FluentBuilder
     {
         return $this->anchors->end();
     }
 
-    public function endOfLine(): self
+    /**
+     * Alias for the `end` method.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function endOfLine(): FluentBuilder
     {
         return $this->end();
     }
