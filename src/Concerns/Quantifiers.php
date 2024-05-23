5ea12f71bb228883f81e2cff20e04f07f228b6de
rudashi <rudashi@gmail.com>
2024-05-23 08:18:16 +0200

    Added comments

--- src/Concerns/Quantifiers.php
+++ src/Concerns/Quantifiers.php
@@ -5,32 +5,54 @@ declare(strict_types=1);
 namespace Rudashi\Concerns;
 
 use LogicException;
+use Rudashi\FluentBuilder;
 use Rudashi\Quantifier;
 
 trait Quantifiers
 {
-    public function zeroOrOne(): static
+    /**
+     * Adds a quantifier that matches once or never.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function zeroOrOne(): FluentBuilder
     {
         $this->pushToPattern(Quantifier::ZERO_OR_ONE);
 
         return $this;
     }
 
-    public function zeroOrMore(): static
+    /**
+     * Adds a quantifier that matches an infinitely many times or not at all.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function zeroOrMore(): FluentBuilder
     {
         $this->pushToPattern(Quantifier::ZERO_OR_MORE);
 
         return $this;
     }
 
-    public function oneOrMore(): static
+    /**
+     * Adds a quantifier that matches once or infinitely many times.
+     *
+     * @return \Rudashi\FluentBuilder
+     */
+    public function oneOrMore(): FluentBuilder
     {
         $this->pushToPattern(Quantifier::ONE_OR_MORE);
 
         return $this;
     }
 
-    public function times(int $number): static
+    /**
+     * Adds a quantifier that matches the given value times.
+     *
+     * @param  int  $number
+     * @return \Rudashi\FluentBuilder
+     */
+    public function times(int $number): FluentBuilder
     {
         if ($number < 0) {
             $this->throwNegativeIntegerException('number');
@@ -41,7 +63,13 @@ trait Quantifiers
         return $this;
     }
 
-    public function min(int $number): static
+    /**
+     * Adds a quantifier that matches a minimum of times.
+     *
+     * @param  int  $number
+     * @return \Rudashi\FluentBuilder
+     */
+    public function min(int $number): FluentBuilder
     {
         if ($number < 0) {
             $this->throwNegativeIntegerException('number');
@@ -52,7 +80,14 @@ trait Quantifiers
         return $this;
     }
 
-    public function between(int $min, int $max = null): static
+    /**
+     * Adds a quantifier that matches between two given values.
+     *
+     * @param  int  $min
+     * @param  int|null  $max
+     * @return \Rudashi\FluentBuilder
+     */
+    public function between(int $min, int $max = null): FluentBuilder
     {
         if ($min < 0) {
             $this->throwNegativeIntegerException('min');
@@ -67,6 +102,14 @@ trait Quantifiers
         return $this;
     }
 
+    /**
+     * Throws a logical exception when used negative integer.
+     *
+     * @param  string  $parameter
+     * @return void
+     *
+     * @throws \LogicException
+     */
     private function throwNegativeIntegerException(string $parameter): void
     {
         throw new LogicException(sprintf('The "%s" parameter must be a positive integer.', $parameter));
